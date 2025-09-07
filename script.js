8e648d30ee3b2f24ab0c18c549d772d4704c4a17
Natsuto-git <baseball10210303@gmail.com>
2025-09-07 17:42:52 +0900

    Initial commit: LotusCard website with floating header

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple header functionality
+document.addEventListener('DOMContentLoaded', function() {
+    // Add any future header functionality here
+    console.log('Header loaded successfully');
+});
